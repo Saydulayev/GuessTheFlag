40d1643deeda69862f1155c467183d3cf6a103e2
Saydulayev <saydulayev.wien@gmail.com>
2023-08-23 09:47:02 +0200

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Dokx Dig on 21.08.23.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
